70ad4e890ff5310ddceb107d716e61333e1c795b
lypegabryel7 <davypegabryel@gmail.com>
2023-05-19 23:12:32 -0300

    Att com exclusão do mapa ant

--- pac.py
+++ pac.py
@@ -1,13 +1,14 @@
 import time
 import random
+import os
 
-num_row = 15
-num_column = 10
+num_row = 13
+num_column = 15
 
-pacman_pos = [random.randint(0, num_column - 1), random.randint(0, num_row-1)]
-comida_pos = [random.randint(0, num_column - 1), random.randint(0, num_row-1)]
+pacman_pos = [random.randint(0, num_row), random.randint(0, num_column)]
+comida_pos = [random.randint(0, num_row), random.randint(0, num_column)]
 
-matriz = [['.'for row in range(num_row)] for column in range(num_column)]
+matriz = [['.'for column in range(num_column)] for row in range(num_row)]
 matriz[pacman_pos[0]][pacman_pos[1]] = 'o'
 matriz[comida_pos[0]][comida_pos[1]] = 'X'
 
@@ -16,4 +17,35 @@ def show_matriz():
         print(' '.join(linha))
     print()
 
-show_matriz()
+def mover_pac():
+    if pacman_pos[0] < comida_pos[0]:
+        pacman_pos[0] += 1
+    elif pacman_pos[0] > comida_pos[0]:
+        pacman_pos[0] -= 1
+    elif pacman_pos[1] < comida_pos[1]:
+        pacman_pos[1] += 1
+    elif pacman_pos[1] > comida_pos[1]:
+        pacman_pos[1] -= 1
+def remove_o(): 
+    if matriz[pacman_pos[0]][pacman_pos[1]-1] == 'o': matriz[pacman_pos[0]][pacman_pos[1]-1] = '.'
+    elif matriz[pacman_pos[0]-1][pacman_pos[1]-1] == 'o': matriz[pacman_pos[0]-1][pacman_pos[1]-1] = '.'
+    elif matriz[pacman_pos[0]-1][pacman_pos[1]] == 'o': matriz[pacman_pos[0]-1][pacman_pos[1]] = '.'
+    elif matriz[pacman_pos[0]+1][pacman_pos[1]] == 'o': matriz[pacman_pos[0]+1][pacman_pos[1]] = '.'
+    elif matriz[pacman_pos[0]+1][pacman_pos[+1]] == 'o': matriz[pacman_pos[0]+1][pacman_pos[+1]] = '.'
+    elif matriz[pacman_pos[0]][pacman_pos[1]+1] == 'o': matriz[pacman_pos[0]][pacman_pos[1]+1] = '.' 
+
+while pacman_pos != comida_pos:
+    
+    mover_pac()
+    
+    pacman_pos[0] = max(0, min(pacman_pos[0], num_column+1)) 
+    pacman_pos[1] = max(0, min(pacman_pos[1], num_row+1)) 
+    matriz[pacman_pos[0]][pacman_pos[1]] = 'O' if pacman_pos == 'o' else 'o'
+    
+    remove_o()
+
+       
+    show_matriz()
+    time.sleep(0.5)
+    os.system('cls')
+print('sim')
