8c670a1a302517b323e65ce8dde450ac8532e239
lypegabryel7 <davypegabryel@gmail.com>
2023-05-23 21:20:02 -0300

    att loop com Esc

--- pac.py
+++ pac.py
@@ -1,17 +1,19 @@
 import time
 import random
 import os
+import keyboard
 
-num_row = 13
-num_column = 15
+num_row = 15
+num_column = 17
 
-pacman_pos = [random.randint(0, num_row), random.randint(0, num_column)]
-comida_pos = [random.randint(0, num_row), random.randint(0, num_column)]
+
 
 matriz = [['.'for column in range(num_column)] for row in range(num_row)]
+pacman_pos = [random.randint(0, num_column), random.randint(0, num_row)]
+comida_pos = [random.randint(0, num_column), random.randint(0, num_row)]
 matriz[pacman_pos[0]][pacman_pos[1]] = 'o'
 matriz[comida_pos[0]][comida_pos[1]] = 'X'
-
+score = 0
 def show_matriz():
     for linha in matriz:
         print(' '.join(linha))
@@ -34,18 +36,17 @@ def remove_o():
     elif matriz[pacman_pos[0]+1][pacman_pos[+1]] == 'o': matriz[pacman_pos[0]+1][pacman_pos[+1]] = '.'
     elif matriz[pacman_pos[0]][pacman_pos[1]+1] == 'o': matriz[pacman_pos[0]][pacman_pos[1]+1] = '.' 
 
-while pacman_pos != comida_pos:
+while keyboard.is_pressed('Esc') == False:
     
     mover_pac()
-    
-    pacman_pos[0] = max(0, min(pacman_pos[0], num_column+1)) 
-    pacman_pos[1] = max(0, min(pacman_pos[1], num_row+1)) 
+     
     matriz[pacman_pos[0]][pacman_pos[1]] = 'O' if pacman_pos == 'o' else 'o'
     
     remove_o()
-
-       
     show_matriz()
-    time.sleep(0.5)
+    time.sleep(0.25)
     os.system('cls')
-print('sim')
+    if pacman_pos == comida_pos: comida_pos = [random.randint(0, num_column-2), random.randint(0, num_row-2)]
+    matriz[comida_pos[0]][comida_pos[1]] = 'X'
+    if pacman_pos == comida_pos: score += 1
+print(f'Parabéns! Sua pontuação foi: {score}')
